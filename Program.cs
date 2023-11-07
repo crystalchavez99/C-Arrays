d01c515d5571cd92ae46cf8b00ccba7f41c17f0a
crystalchavez99 <redfire1205@hotmail.com>
2023-11-06 16:41:05 -0800

    out of bound and read index

--- Program.cs
+++ Program.cs
@@ -3,3 +3,10 @@
 fraudulentOrderIDs[0] = "A123";
 fraudulentOrderIDs[1] ="B456";
 fraudulentOrderIDs[2] = "C789";
+
+// breaks app
+//fraudulentOrderIDs[3] = "D000";
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
