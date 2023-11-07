0da88f322b92a8eca6702bbacde2d353e7354c0f
crystalchavez99 <redfire1205@hotmail.com>
2023-11-06 16:45:41 -0800

    length

--- Program.cs
+++ Program.cs
@@ -16,3 +16,11 @@
 // Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
 string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
